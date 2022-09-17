98a99bc446eddf5122b51f89790f0e34df4e7109
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-09-18 00:18:50 +0300

    Implemented useEffect to Show own IP address on initial load & Made Style adjustments & updated ReadMe

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ Integrating [IP Geolocation API by IPify](https://geo.ipify.org/) & LeafletJS li
     * <strong>Built with</strong>
 ### App. Functionalities
 * <strong>Search</strong> for any IP addresses & see the key information and marker location on the map
-* Compatible layout for mobile screen width
+* See own IP Address on the map on the initial load
 ### Screenshots
-![image](https://user-images.githubusercontent.com/89903354/185748966-77666395-bc93-4c1e-ab07-83a26e9b3aed.png)
-![image](https://user-images.githubusercontent.com/89903354/185749029-fd2e6bc8-3809-4330-878a-c3f174983fc1.png)
+![image](https://user-images.githubusercontent.com/89903354/190876671-0f3eac82-1ebd-4bb2-b492-a72ccb29be68.png)
+![image](https://user-images.githubusercontent.com/89903354/190876691-43abeddf-0d65-4a7c-9b68-c7911fb8828b.png)
+![image](https://user-images.githubusercontent.com/89903354/190876706-2670cff2-2b88-4925-b2fa-830007918b2b.png)
 ### Built With
 * <strong>React - JS library</strong>
 * <strong>TypeScript</strong>
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import Arrow from './assets/icon-arrow.svg';
 import axios from 'axios';
 import Map from './components/Map';
@@ -24,6 +24,23 @@ function App() {
         lng: 0,
     });
 
+    useEffect(() => {
+        getLocationOnLoad();
+    }, [])
+
+    const getLocationOnLoad = async () => {
+        const apiKey = 'at_8mNQ5ojf6nhZByp4NIj0O8e9PJXr8';
+        const { data } = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`);
+        setData({
+            ip: data.ip,
+            address: data.location.city,
+            timezone: data.location.timezone,
+            isp: data.isp,
+            lat: data.location.lat,
+            lng: data.location.lng,
+        })
+    };
+
     const getLocationData = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const apiKey = 'at_8mNQ5ojf6nhZByp4NIj0O8e9PJXr8';
@@ -41,51 +58,51 @@ function App() {
     return (
     <div className="h-screen w-full">
         <div className="bg-pattern-texture bg-cover w-full h-1/4 relative z-50">
-            <div className="flex justify-center text-white text-4xl font-medium">
+            <div className="flex justify-center text-white sm:text-4xl text-2xl font-medium">
                 <h1 className="mt-10">
                     IP Address Tracker
                 </h1>
             </div>
             <div className="flex justify-center mt-10 w-full">
-                <form  onSubmit={getLocationData} className="w-1/3">
+                <form  onSubmit={getLocationData} className="sm:w-1/3 w-4/5">
                     <input className="w-full p-4 rounded-xl relative"
                            placeholder="Search for any IP adress or domain"
                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setLocation(e.target.value)}}
                     />
-                    <button type='submit' className="float-right bg-sky-400 absolute p-4 h-14 -ml-4 rounded-tr-lg rounded-br-lg bg-slate-900">
+                    <button type='submit' className="float-right bg-sky-400 absolute p-4 h-14 sm:-ml-4 -ml-10 rounded-tr-lg rounded-br-lg bg-slate-900">
                         <img src={Arrow} alt="arrow"/>
                     </button>
                 </form>
             </div>
-            <div className="sm:h-44 h-auto bg-white sm:w-2/3  w-4/5 mt-16 rounded-lg absolute left-0 right-0 m-auto">
-                <div className="flex sm:justify-between items-center sm:flex-row flex-col text-xs font-bold text-gray-500 sm:whitespace-normal whitespace-nowrap">
-                    <div className="sm:ml-10 mt-10 w-1/5">
+            <div className="sm:h-44 h-auto bg-white sm:w-2/3 w-4/5 mt-16 rounded-lg absolute left-0 right-0 m-auto">
+                <div className="flex sm:justify-between items-center sm:flex-row flex-col text-sm font-bold text-gray-500 sm:whitespace-normal whitespace-nowrap">
+                    <div className="w-full sm:ml-10 sm:mt-12 mt-4 sm:w-1/5 text-center sm:text-left">
                        <p>IP ADDRESS</p>
-                        <div className="mt-4 sm:text-xl text-lg text-black w-full">
+                        <div className="mt-4 sm:text-2xl text-xl text-black w-full">
                             <h1>
                                 {ipData.ip}
                             </h1>
                         </div>
                     </div>
-                    <div className="mt-10 w-1/5">
+                    <div className="w-full sm:mt-12 mt-4 sm:w-1/5 text-center sm:text-left">
                         <p>LOCATION</p>
-                        <div className="mt-4 sm:text-xl text-lg text-black">
+                        <div className="mt-4 sm:text-2xl text-xl text-black">
                             <h1>
                                 {ipData.address}
                             </h1>
                         </div>
                     </div>
-                    <div className="mt-10 w-1/5">
+                    <div className="w-full sm:mt-12 mt-4 sm:w-1/5 text-center sm:text-left">
                         <p>TIMEZONE</p>
-                        <div className="mt-4 sm:text-xl text-lg text-black">
+                        <div className="mt-4 sm:text-2xl text-xl text-black">
                             <h1>
                                 {ipData.timezone}
                             </h1>
                         </div>
                     </div>
-                    <div className="mt-10 w-1/5">
-                        <p>ISP</p>
-                        <div className="mt-4 sm:text-xl text-lg text-black sm:mb-0 mb-2">
+                    <div className="w-full sm:mt-12 mt-4 sm:w-1/5 text-center sm:text-left">
+                        <p className="">ISP</p>
+                        <div className="mt-4 sm:text-2xl text-xl text-black sm:mb-0 mb-2">
                             <h1>
                                 {ipData.isp}
                             </h1>
